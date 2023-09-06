431699029561d87e639cd627ce03c8f89c49003e
unknown <Munezero2000>
2023-09-06 16:00:37 +0200

    added rain bow color

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-let playground = document.querySelector(".playground");
+let value = prompt("Enter size: ");
+document.querySelector("#slider").value
 
+
+let rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+function generateRandomColor() {
+  let color = Math.floor(Math.random() * rainbow.length);
+  return color;
+}
+
+//
+let playground = document.querySelector(".playground");
+playground.style.display = "grid";
+playground.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+playground.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+
+//
 function createDivs(size) {
-  for (i = 1; i <= size; i++) {
+  for (i = 1; i <= size ** 2; i++) {
     let innerdiv = document.createElement("div");
     innerdiv.classList.add("innerdiv");
     playground.appendChild(innerdiv);
   }
 }
-createDivs(256);
+createDivs(value);
 
+//
 document.querySelectorAll(".innerdiv").forEach((div) => {
-  div.addEventListener("click", (e) => {
-    e.target.style.backgroundColor = "black";
+  div.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = `${rainbow[generateRandomColor()]}`;
   });
 });
